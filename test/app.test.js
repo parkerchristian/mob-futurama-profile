1e6d03525ddf25eb1e6abe8ca81980d53ba012ee
Parker Helland <hellandp@gmail.com>
2019-04-15 17:49:11 -0700

    in the process of adding tagline from favCharacter

--- test/app.test.js
+++ test/app.test.js
@@ -1,20 +1,25 @@
 const request = require('supertest');
 const app = require('../lib/app');
-//const Profile = require('../lib/models/Profile');
+const Profile = require('../lib/models/Profile');
 
-describe('APP ROUTES', () => {
+describe.skip('APP ROUTES', () => {
+  afterEach(() => {
+    return Profile.drop();
+  });
 
   it('create profile', () => {
     return request(app)
       .post('/profiles')
       .send({
         name: 'Mob',
-        favCharacter: 'Jimmy',
+        favCharacter: 'Bender',
       })
       .then(res => {
+        // console.log('**', res.body);
         expect(res.body).toEqual({
           name: 'Mob',
-          favCharacter: 'Jimmy',
+          favCharacter: 'Bender',
+          tagline: expect.any(String),
           _id: expect.any(String)
         });
       });
